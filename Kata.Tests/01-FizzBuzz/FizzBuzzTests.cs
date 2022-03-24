06474a8d809dec2a23450561be41ef10f427446d
suparuksa <suparuksa.dev@gmail.com>
2022-03-24 13:00:09 +0700

    red 2

--- Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
+++ Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
@@ -2,15 +2,17 @@
 namespace Kata.FizzBuzz.Tests;
 public class FizzBuzzTests
 {
-    [Fact]
-    public void TestGetFizzBuzz()
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(2, "2")]
+    public void TestGetFizzBuzz(int input, string expected)
     {
         // Arrange
         var sut = new FizzBuzz();
         // Act
-        var actual = sut.SayFizzBuzz(1);
+        var actual = sut.SayFizzBuzz(input);
         // Assert
-        actual.Should().Be("1");
+        actual.Should().Be(expected);
     }
 }
 
