4ad0e0633375c53d34c46f1f43904edd924605cc
suparuksa <suparuksa.dev@gmail.com>
2022-03-24 13:09:09 +0700

    green

--- Kata/01-FizzBuzz/FizzBuzz.cs
+++ Kata/01-FizzBuzz/FizzBuzz.cs
@@ -4,7 +4,7 @@ public class FizzBuzz
 {
     public string SayFizzBuzz(int number)
     {
-        if (number == 3)
+        if (number % 3 == 0)
             return "Fizz";
         else
             return number.ToString();
