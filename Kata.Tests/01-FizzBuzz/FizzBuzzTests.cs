87a60ab2924cfcac58346d5b2c73327188777732
suparuksa <suparuksa.dev@gmail.com>
2022-03-24 13:22:14 +0700

    fizzbuzz red 1

--- Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
+++ Kata.Tests/01-FizzBuzz/FizzBuzzTests.cs
@@ -3,13 +3,17 @@ namespace Kata.FizzBuzz.Tests;
 public class FizzBuzzTests
 {
     [Theory]
+    //normal number
     [InlineData(1, "1")]
     [InlineData(2, "2")]
+    //fizz number
     [InlineData(3, "Fizz")]
     [InlineData(6, "Fizz")]
-    //
+    //buzz number
     [InlineData(5, "Buzz")]
     [InlineData(10, "Buzz")]
+    //fizzbuzz number
+    [InlineData(15, "FizzBuzz")]
 
     public void TestGetFizzBuzz(int input, string expected)
     {
